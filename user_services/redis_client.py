de61595c6baf81be71a354db33749a567d92d667
yanghanggit <1220826563@qq.com>
2025-05-27 17:41:45 +0800

    先临时过了严格模式检查。后续再说。

--- user_services/redis_client.py
+++ user_services/redis_client.py
@@ -1,17 +1,16 @@
 # utils/redis_client.py
 import redis
 from fastapi import Depends
+from typing import Any
 
-def get_redis():
+
+def get_redis() -> Any:
     pool = redis.ConnectionPool(
-        host='localhost',
-        port=6379,
-        db=0,
-        decode_responses=True,
-        max_connections=20
+        host="localhost", port=6379, db=0, decode_responses=True, max_connections=20
     )
     return redis.Redis(connection_pool=pool)
 
+
 # 使用示例
-async def get_session_data(session_id: str, redis: redis.Redis = Depends(get_redis)):
+async def get_session_data(session_id: str, redis: Any = Depends(get_redis)) -> Any:
     return redis.hgetall(f"session:{session_id}")
